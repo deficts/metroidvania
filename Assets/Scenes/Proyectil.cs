71c134e2c63f0410e7b067880c4ac43d595c08b3
Nicole Carrllo <42300492+NicoleCarrillo@users.noreply.github.com>
2020-04-07 12:51:43 -0500

    Disparo enemigo recto

---
+++ Assets/Scenes/Proyectil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectil : MonoBehaviour
+{
+
+    
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb=GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up*11,ForceMode2D.Impulse);
+        Destroy(gameObject,3);
+
+    }
+
+    
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag=="PLAYER"){
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+
+    }
+}
