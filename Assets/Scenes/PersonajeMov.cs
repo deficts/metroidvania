cff56ebbf7e7a4bd1673eb1164e98daf38e2fe1b
Nicole Carrllo <42300492+NicoleCarrillo@users.noreply.github.com>
2020-04-07 10:57:47 -0500

    Mecanismo de vida

--- Assets/Scenes/PersonajeMov.cs
+++ Assets/Scenes/PersonajeMov.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PersonajeMov : MonoBehaviour
 {
     //private Animator animator;  //ANIMACION DEL PERSONAJE
     Rigidbody2D rb;
     float dirX, moveSpeed=5f;
+    public Text texto;
+    public int puntuacion = 100;
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        texto.text = "VIDA: "+puntuacion;
         //animator = GetComponent<Animator>();   PARA ANIMACION
     }
 
@@ -26,6 +30,8 @@ public class PersonajeMov : MonoBehaviour
         if(Input.GetButtonDown("Jump")&&rb.velocity.y==0)           //SALTO 
             rb.AddForce(Vector2.up*150f);
 
+        mecanismoVida(puntuacion);              //SIEMPRE LO REVISA
+
     }
 
     public void FixedUpdate(){
@@ -33,15 +39,31 @@ public class PersonajeMov : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D collision){                 //PERSONAJE SIGUE EL MOVIMIENTO DE LAS PLATAFORMAS
-     if(collision.gameObject.tag=="Plataforma"){
+    if(collision.gameObject.tag=="Plataforma"){
         this.transform.parent=collision.transform;
-        }
+     }
+
+    if (collision.gameObject.tag == "Bala"){            //MECANISMO DE VIDA
+        puntuacion = puntuacion -10 ;
+    }
+
+     if (collision.gameObject.tag == "Malo"){                   //MECANISMO DE VIDA
+       puntuacion = puntuacion -50 ;
+    }
+
+        texto.text = "VIDA: "+puntuacion.ToString();
     }
 
     private void OnCollisionExit2D(Collision2D collision){
  
      if(collision.gameObject.tag=="Plataforma"){
         this.transform.parent=null;
+        }
+    }
+
+    public void mecanismoVida(int puntuacion){          //SE DETIENE EL JUEGO 
+        if(puntuacion<=0){
+            Time.timeScale = 0;
         }
     }
 
