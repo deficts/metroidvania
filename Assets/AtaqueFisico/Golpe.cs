97f9631c0248fdfe1f4e1b4ba48229f7018f2fd4
luisenamm <a01633894@itesm.mx>
2020-04-07 19:01:40 -0500

    Implementando ataque fisico

---
+++ Assets/AtaqueFisico/Golpe.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golpe : MonoBehaviour
+{
+    public float vida=100.0f;
+    public GameObject player;
+    public Transform referencia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ataque();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+        if(Input.GetKeyDown(KeyCode.G)){
+           Ataque();
+        }
+       
+    }
+
+    void Ataque(){
+        if(Vector3.Distance(player.transform.position,transform.position)<0.3f){
+            vida--;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Ataque();
+    }
+}
