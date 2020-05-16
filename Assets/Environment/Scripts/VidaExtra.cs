7e6e1da02e5c38a9b6f1946301e464111302871a
deficts <diegoar7@live.com>
2020-05-15 20:18:34 -0500

    Agregando prefab de vida extra funcional

---
+++ Assets/Environment/Scripts/VidaExtra.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VidaExtra : MonoBehaviour
+{
+    public PlayerManager player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (player.vida + 50 >= 100)
+            {
+                player.vida = 100;
+            }
+            else
+            {
+                player.vida += 50;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
