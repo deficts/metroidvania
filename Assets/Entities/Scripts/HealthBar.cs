76b3259eb476b77778d221d808fa491896584626
luisenamm <30873889+luisenamm@users.noreply.github.com>
2020-06-03 20:27:37 -0500

    Vida Gráfica

--- Assets/Entities/Scripts/HealthBar.cs
+++ Assets/Entities/Scripts/HealthBar.cs
@@ -1,22 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     public Image Bar;
     public PlayerManager player;
-    private int vidaBarra;
+    private float vidaBarra;
     
     // Start is called before the first frame update
     void Start()
     {
-        vidaBarra=player.vida/100;
+        vidaBarra=(float)player.vida/100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        vidaBarra=player.vida/100;
+        vidaBarra=(float)player.vida/100;
         Bar.fillAmount=vidaBarra;
     }
 }
